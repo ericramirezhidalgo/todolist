79dd062336525e7f27c23056d0f6f9b59deef25b
ericramirezhidalgo <ericramirezhidalgo@gmail.com>
2024-12-04 15:16:38 +0100

    add: UI and CRUD

--- myapp/app.js
+++ myapp/app.js
@@ -1,18 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 3005;
 const { body, validationResult } = require('express-validator');
+
+const port = process.env.PORT || 3005;
+
+app.use(cors({ origin: 'http://localhost:3006' }));
 app.use(express.json());
 
 const todos = [
-    {
-        id: 1, title: "Aprender Express.js", completed: false
-    },
-    {
-        id: 2, title: "Revisar middleware", completed: true
-    }
+    { id: 0, title: "Aprender Express.js", completed: false },
+
 ];
 
 const loggingMiddleware = (req, res, next) => {
@@ -45,17 +45,16 @@ app.get('/todos', (req, res) => {
     res.status(201).send(todos);
 });
 
-app.post('/todos', body('title').notEmpty().withMessage('Title cannot be empty'), (req, res) => 
-    { 
-       const errors = validationResult(req);
-       if (!errors.isEmpty()) { return res.status(400).json({ errors: errors.array() }); }
-       const { body } = req; 
-       const newTodo = { id: todos.length ? todos[todos.length - 1].id + 1 : 1, ...body, completed: false };
-       todos.push(newTodo); res.status(201).send(newTodo);
- });
+app.post('/todos', body('title').notEmpty().withMessage('Title cannot be empty'), (req, res) => { 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) { return res.status(400).json({ errors: errors.array() }); }
+    const { body } = req; 
+    const newTodo = { id: todos.length ? todos[todos.length - 1].id + 1 : 1, ...body, completed: false };
+    todos.push(newTodo);
+    res.status(201).send(newTodo);
+});
 
-app.put('/todos/:id', body('completed').isBoolean().withMessage('Completed must be a boolean'), (req, res) =>
-    {
+app.put('/todos/:id', body('completed').isBoolean().withMessage('Completed must be a boolean'), (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) { return res.status(400).json({ errors: errors.array() }); }
     const { body, params: { id } } = req;
@@ -65,17 +64,18 @@ app.put('/todos/:id', body('completed').isBoolean().withMessage('Completed must 
     if (findTodoIndex === -1) return res.status(404).json({ error: 'Todo not found' });
     todos[findTodoIndex] = { ...todos[findTodoIndex], completed: body.completed };
     return res.status(200).json(todos[findTodoIndex]); 
-}); 
-app.delete('/todos/:id', (req, res) => 
-{
-    const { params: { id } } = req; const parsedId = parseInt(id);
+});
+
+app.delete('/todos/:id', (req, res) => {
+    const { params: { id } } = req; 
+    const parsedId = parseInt(id);
     if (isNaN(parsedId)) return res.sendStatus(400);
     const findTodoIndex = todos.findIndex((todo) => todo.id === parsedId);
     if (findTodoIndex === -1) return res.status(404).json({ error: 'Todo not found' });
     todos.splice(findTodoIndex, 1);
     const message = { message: "Tarea eliminada" }; 
     return res.status(200).send(message);
-   });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
